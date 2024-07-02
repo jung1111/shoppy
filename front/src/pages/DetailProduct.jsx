f4c5e2bd8353ac2dd30097c2e8d722d6a3cfa5dd
jung1111 <chobo0799@naver.com>
2024-07-02 14:38:22 +0900

    detail page cart add

--- front/src/pages/DetailProduct.jsx
+++ front/src/pages/DetailProduct.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import axios from 'axios';
+import { useNavigate, useParams } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { getUser } from '../util/localStorage'
 import { productDetail } from '../modules/reduxProductsAxios';
@@ -8,17 +7,12 @@ import { cartItemAdd } from '../modules/reduxCartsAxios';
 
 
 export default function DetailProduct({addCartCount}) { /** GET : url을 통해 넘어오는 파라미터는 useParams hook!!  */
+  const navigate = useNavigate();
   const dispatch = useDispatch();
   const { id } = useParams();  // 상품아이디
-  const product = useSelector(state => state.products.item);
-
-  // const [product, setProduct] = useState({});    
+  const isLogin = useSelector(state => state.member.isLogin);
+  const product = useSelector(state => state.products.item);    
   const [size, setSize] = useState('xs');
-
-  const cnt = useSelector(state => state.carts.cnt);
-
-  // console.log('cnt ->',cnt);
-
 
   useEffect(()=>{
     dispatch(productDetail({id, size}))
@@ -27,11 +21,20 @@ export default function DetailProduct({addCartCount}) { /** GET : url을 통해 
 	
   //장바구니 추가
   const handleAddCart = (id) => {
-    const userId = getUser().userId;
-    dispatch(cartItemAdd({id, size, userId}))
+    const userInfo = getUser();
+
+    if(isLogin || userInfo !== null){
+      const userId = userInfo.userId;
+      dispatch(cartItemAdd({id, size, userId}))
+      window.location.reload();
+    }else {
+      alert('로그인이 필요한 기능입니다.')
+      navigate('/login');
+    }
+    
   }
 
-  // console.log(size)
+
     return (
       <div className='content'>
         <div className='product-detail'>
